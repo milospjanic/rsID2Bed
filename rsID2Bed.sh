2bd927ef891cfd01cdaf7386d97b8002a8543673
Milos Pjanic <pjanic.milos@gmail.com>
2016-12-13 15:42:35 -0800

    Update rsID2Bed.sh

--- rsID2Bed.sh
+++ rsID2Bed.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 FILE=~/rsID2Bed/snp149Common.bed
+DIR=~/rsID2Bed
 
-if 
+if [ ! -d $DIR ]
 then
 mkdir ~/rsID2Bed
 fi
@@ -11,5 +12,5 @@ cd ~/rsID2Bed
 
 if [ ! -f $FILE ]
 then
-mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -N -D hg19 -e 'SELECT chrom, chromStart, chromEnd, name FROM snp149Common' > snp149Common.bed
+mysql --user=genome --host=genome-mysql.cse.ucsc.edu -A -N -D hg19 -e 'SELECT chrom, chromStart, chromEnd, name FROM snp147Common' > snp147Common.bed
 fi
